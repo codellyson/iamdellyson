c1a16d76b4512269069fa0b6c4263f6888a66fbf
Isiaka Lukman Bamidele <isiakalukmandellyson@gmail.com>
2022-03-20 13:54:37 +0100

    completed design[added a fixed]

--- pages/Layout/Layout.js
+++ pages/Layout/Layout.js
@@ -1,15 +1,8 @@
-import Image from "next/image";
+import Head from "next/head";
 import React from "react";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 function Layout({ children, ...rest }) {
-  React.useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://cdn.splitbee.io/sb.js";
-
-    document.body.appendChild(script);
-  }, []);
-
   return (
     <div className="page-wrapper">
       <Navbar />
